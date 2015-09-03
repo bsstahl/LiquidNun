4e990f7a0b96da4c40ee140d6bc693e6d08f1427
Barry Stahl <bss_github@bsstahl.com>
2015-09-03 16:30:28 -0700

    Create the ITimingProvider interface. Issues: Closes #14.

---
+++ LiquidNun/Interfaces/ITimingProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiquidNun.Interfaces
+{
+    /// <summary>
+    /// Describes the interface of a service that accepts times and time spans in
+    /// various forms and synchronously returns at the appropriate time, thus
+    /// providing a "delay service".
+    /// </summary>
+    public interface ITimingProvider
+    {
+        /// <summary>
+        /// Delays for the specified amount of time.
+        /// </summary>
+        /// <param name="length">The length of time after which the method should return.</param>
+        void Delay(TimeSpan length);
+
+        /// <summary>
+        /// Delays until the specified UTC time.
+        /// </summary>
+        /// <param name="untilUtc">The UTC time at which the method should return.</param>
+        void Delay(DateTime untilUtc);
+
+        /// <summary>
+        /// Delays until the specified absolute time (time with timezone offset).
+        /// </summary>
+        /// <param name="until">The time, with timezone information, at which the method should return.</param>
+        void Delay(DateTimeOffset until);
+    }
+}
