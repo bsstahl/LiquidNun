0567bfc35c0eaedba0a4c449b0074830e1069f57
Barry Stahl <bss_github@bsstahl.com>
2015-09-03 16:39:12 -0700

    Stub the Threading Timing provider.

---
+++ LiquidNun.Timing.Threading/Provider.cs
@@ -0,0 +1,42 @@
+﻿using LiquidNun.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiquidNun.Timing.Threading
+{
+    /// <summary>
+    /// Implements a delay service using the System.Threading API
+    /// in the Microsoft .NET Framework.
+    /// </summary>
+    public class Provider : ITimingProvider
+    {
+        /// <summary>
+        /// Delays until the specified absolute time (time with timezone offset).
+        /// </summary>
+        /// <param name="until">The time, with timezone information, at which the method should return.</param>
+        public void Delay(DateTimeOffset until)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Delays until the specified UTC time.
+        /// </summary>
+        /// <param name="untilUtc">The UTC time at which the method should return.</param>
+        public void Delay(DateTime untilUtc)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Delays for the specified amount of time.
+        /// </summary>
+        /// <param name="length">The length of time after which the method should return.</param>
+        public void Delay(TimeSpan length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
