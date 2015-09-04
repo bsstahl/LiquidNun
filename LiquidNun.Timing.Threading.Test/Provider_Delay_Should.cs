cdb105acc025d4d8149c65be543137bb0a468188
Barry Stahl <bss_github@bsstahl.com>
2015-09-03 17:02:16 -0700

    Provider_Delay_Should.ReturnAfterTheCorrectDelayIfADelayTimespanIsSpecified - Red

---
+++ LiquidNun.Timing.Threading.Test/Provider_Delay_Should.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelperExtensions;
+
+namespace LiquidNun.Timing.Threading.Test
+{
+    [TestClass]
+    public class Provider_Delay_Should
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void ReturnAfterTheCorrectDelayIfADelayTimespanIsSpecified()
+        {
+            var target = new LiquidNun.Timing.Threading.Provider();
+            var delayInMs = 100.GetRandom(50);
+            var delayTimespan = TimeSpan.FromMilliseconds(delayInMs);
+
+            var timer = new System.Diagnostics.Stopwatch();
+            timer.Start();
+            target.Delay(delayTimespan);
+            timer.Stop();
+
+            Assert.AreEqual(delayInMs, timer.ElapsedMilliseconds);
+        }
+    }
+}
