e88e2d2f04aad26430d761990ac6fc2366502ef2
Barry Stahl <barry@bsstahl.com>
2017-01-03 16:58:00 -0700

    Add reliable messaging abstractions (no implementation yet)

---
+++ LiquidNun/Enumerations/Priority.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiquidNun.Enumerations
+{
+    /// <summary>
+    /// Represents the importance of an item with the lowest value 
+    /// representing the lowest importance.
+    /// </summary>
+    public enum Priority
+    {
+        /// <summary>
+        /// Represents the minimum possible priority 
+        /// </summary>
+        Lowest = 0,
+
+        /// <summary>
+        /// Represents an extremely low, but not the lowest priority
+        /// </summary>
+        VeryLow = 1,
+
+        /// <summary>
+        /// Represents a below average priority
+        /// </summary>
+        Low = 2,
+
+        /// <summary>
+        /// Represents the standard (default) priority
+        /// </summary>
+        Normal = 3,
+
+        /// <summary>
+        /// Represents a slightly above-average priority
+        /// </summary>
+        AboveNormal = 4,
+
+        /// <summary>
+        /// Represents a high, but not nearly the highest priority
+        /// </summary>
+        High = 5,
+
+        /// <summary>
+        /// Represents an extremely high, but not the highest priority
+        /// </summary>
+        VeryHigh = 6,
+
+        /// <summary>
+        /// Represents the minimum possible priority 
+        /// </summary>
+        Highest = 7
+    }
+}
