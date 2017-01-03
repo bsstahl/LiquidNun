32a4512a0ee375b3ea488ef402d3da0a56d860f6
Barry Stahl <barry@bsstahl.com>
2017-01-03 15:50:42 -0700

    Cleanup some build errors due to Core tooling V1.0.1

--- LiquidNun.Directory.FileSystem/Properties/AssemblyInfo.cs
+++ LiquidNun.Directory.FileSystem/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("US Airways")]
+[assembly: AssemblyCompany("Barry S. Stahl")]
 [assembly: AssemblyProduct("LiquidNun.Directory.FileSystem")]
 [assembly: AssemblyTrademark("")]
 
