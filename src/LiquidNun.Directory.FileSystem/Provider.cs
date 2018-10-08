6afeaf14fa791af892ba7499ab81f06aa850bcdb
bsstahl <barry@bsstahl.com>
2018-10-07 22:09:04 -0700

    Update DirectoryService to NetStandard2.0

---
+++ src/LiquidNun.Directory.FileSystem/Provider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LiquidNun.Interfaces;
+
+namespace LiquidNun.Directory.FileSystem
+{
+    public class Provider : Interfaces.IDirectoryService
+    {
+        #region IDirectoryService Members
+
+        public string ReadAllText(string filePath)
+        {
+            if (!System.IO.File.Exists(filePath))
+                throw new Exceptions.FileNotFoundException(filePath);
+            return System.IO.File.ReadAllText(filePath);
+        }
+
+        public IEnumerable<string> GetFiles(string pathName)
+        {
+            try
+            {
+                return System.IO.Directory.GetFiles(pathName);
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                throw new Exceptions.DirectoryNotFoundException(pathName);
+            }
+        }
+
+        public bool Exists(string pathName)
+        {
+            return System.IO.Directory.Exists(pathName);
+        }
+
+        public bool FileExists(string filePath)
+        {
+            return System.IO.File.Exists(filePath);
+        }
+
+        #endregion
+    }
+
+}
---
+++ src/LiquidNun/Interfaces/IDirectoryService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LiquidNun.Interfaces
+{
+    /// <summary>
+    /// Describes an interface of a provider that implements directory 
+    /// services for files and other file system type objects.
+    /// </summary>
+    public interface IDirectoryService
+    {
+        /// <summary>
+        /// Returns the names of files in the specified directory.
+        /// </summary>
+        /// <param name="pathName">Path name.</param>
+        IEnumerable<string> GetFiles(string pathName);
+
+        //void MoveFile(string sourceFilePath, string destinationFolderPath);
+        //void MoveFile(string sourceFilePath, string destinationFolderPath, string destinationFileName);
+
+        //void CopyFile(string sourceFilePath, string destinationFolderPath);
+        //void CopyFile(string sourceFilePath, string destinationFolderPath, string destinationFileName);
+        //void CopyFile(string sourceFilePath, string destinationFolderPath, bool overwrite);
+        //void CopyFile(string sourceFilePath, string destinationFolderPath, string destinationFileName, bool overwrite);
+
+        /// <summary>
+        /// Returns all of the text of the file at the specified path
+        /// </summary>
+        /// <param name="filePath">Path name.</param>
+        string ReadAllText(string filePath);
+
+        //string[] ReadAllLines(string filePath);
+        //void DeleteFile(string filePath);
+        bool FileExists(string filePath);
+        //byte[] ReadAllBytes(string filePath);
+        //System.IO.Stream GetFileData(string filePath);
+
+        bool Exists(string pathName);
+    }
+}
