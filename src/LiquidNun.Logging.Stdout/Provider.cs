184fb94f308ff8bfe1a0e87e8d1688d8cae5bea8
bsstahl <barry@bsstahl.com>
2018-10-07 21:53:33 -0700

    Convert to NetStandard2.0

---
+++ src/LiquidNun.Logging.Stdout/Provider.cs
@@ -0,0 +1,28 @@
+﻿using LiquidNun.Entities;
+using LiquidNun.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiquidNun.Logging.GenericSink
+{
+    /// <summary>
+    /// Logs the specified events to the Console
+    /// </summary>
+    public class Provider : LoggingProviderBase
+    {
+        /// <summary>
+        /// Writes the log entry into the sink
+        /// </summary>
+        /// <param name="category">The application specific category of the log entry.</param>
+        /// <param name="title">The title for the log entry. This is usually the 1st thing that 
+        /// will be seen when entries are reviewed.</param>
+        /// <param name="message">The full text of the message sent to the logging provider.</param>
+        /// <param name="severity">An indication of how critical an event the entry represents.</param>
+        /// <param name="entryType">The type of entry as described in the EventLogEntryType enumeration.</param>
+        public override void Write(string category, string title, string message, int severity, EventLogEntryType entryType)
+        {
+            Console.WriteLine("Logger: {0} ({1}/{3}) - {2}\r\n", title, category, message, severity);
+        }
+    }
+}
---
+++ tst/LiquidNun.Timing.Threads.Test/Provider_Delay_Should.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using TestHelperExtensions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace LiquidNun.Timing.Threads.Test
+{
+    public class Provider_Delay_Should
+    {
+        [Fact]
+        public void AlwaysDelayAtLeastAsLongAsTheSpecifiedValue()
+        {
+            const int executionCount = 100;
+
+            var target = new Threads.Provider();
+            var maxDelayValue = 25;
+            var minDelayValue = 15;
+
+            var delayInMs = maxDelayValue.GetRandom(minDelayValue);
+            var delayTimespan = TimeSpan.FromMilliseconds(delayInMs);
+
+            var results = new List<long>();
+
+            for (int i = 0; i < executionCount; i++)
+            {
+                var timer = new System.Diagnostics.Stopwatch();
+                timer.Start();
+                target.Delay(delayTimespan);
+                timer.Stop();
+                results.Add(timer.ElapsedMilliseconds);
+            }
+
+            Console.WriteLine($"Specified: {delayTimespan.Milliseconds}  Min Actual: {results.Min(m => m)}");
+            Console.WriteLine($"Max Actual: {results.Max(m => m)}    Average: {results.Average()}");
+            Assert.DoesNotContain(results, m => m < delayInMs);
+        }
+
+        [Fact]
+        public void AlwaysDelayAtLeastAsLongAsTheSpecifiedValueForLongerValues()
+        {
+            const int executionCount = 10;
+
+            var target = new Threads.Provider();
+            var maxDelayValue = 250;
+            var minDelayValue = 150;
+
+            var delayInMs = maxDelayValue.GetRandom(minDelayValue);
+            var delayTimespan = TimeSpan.FromMilliseconds(delayInMs);
+
+            var results = new List<long>();
+
+            for (int i = 0; i < executionCount; i++)
+            {
+                var timer = new System.Diagnostics.Stopwatch();
+                timer.Start();
+                target.Delay(delayTimespan);
+                timer.Stop();
+                results.Add(timer.ElapsedMilliseconds);
+            }
+
+            Console.WriteLine($"Specified: {delayTimespan.Milliseconds}  Min Actual: {results.Min(m => m)}");
+            Console.WriteLine($"Max Actual: {results.Max(m => m)}    Average: {results.Average()}");
+            Assert.DoesNotContain(results, m => m < delayInMs);
+        }
+
+        [Fact]
+        public void NeverDelayLongerThanTheToleranceIfADelayTimespanIsSpecified()
+        {
+            // This is only a sanity-test.  I don't need to check
+            // that Microsoft's Delay API works properly, I just need
+            // to make sure that I called it properly so that I
+            // am getting reasonable results.
+
+            const int executionCount = 100;
+            const double tolerance = 0.67;  // 50% tolerance at the high-end
+
+            var target = new Threads.Provider();
+            var maxDelayValue = 25;
+            var minDelayValue = 15;
+
+            var delayInMs = maxDelayValue.GetRandom(minDelayValue);
+            var maxDelayRange = delayInMs * (tolerance + 1.0);
+            var delayTimespan = TimeSpan.FromMilliseconds(delayInMs);
+
+            var results = new List<long>();
+
+            for (int i = 0; i < executionCount; i++)
+            {
+                var timer = new System.Diagnostics.Stopwatch();
+                timer.Start();
+                target.Delay(delayTimespan);
+                timer.Stop();
+                results.Add(timer.ElapsedMilliseconds);
+            }
+
+            Console.WriteLine($"Specified: {delayTimespan.Milliseconds}  Max Allowed: {maxDelayRange}  Max Actual: {results.Max(m => m)}");
+            Assert.DoesNotContain(results, m => m > maxDelayRange);
+        }
+    }
+}
