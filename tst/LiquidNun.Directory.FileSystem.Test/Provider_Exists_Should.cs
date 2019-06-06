a84aa10c32d2d9d6af8200191812501a2f54e96b
Barry Stahl <bss_github@bsstahl.com>
2019-06-06 14:22:59 -0700

    FileSystem Directory provider Exists test

--- tst/LiquidNun.Directory.FileSystem.Test/Provider_Exists_Should.cs
+++ tst/LiquidNun.Directory.FileSystem.Test/Provider_Exists_Should.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TestHelperExtensions;
 using Xunit;
 
 namespace LiquidNun.Directory.FileSystem.Test
@@ -8,9 +9,19 @@ namespace LiquidNun.Directory.FileSystem.Test
     public class Provider_Exists_Should
     {
         [Fact]
-        public void Test1()
+        public void ReturnFalseIfThePathDoesNotExist()
         {
-            throw new NotImplementedException();
+            string filePath = $"{string.Empty.GetRandom()}\\{string.Empty.GetRandom()}.txt";
+            var target = new Provider();
+            Assert.False(target.Exists(filePath));
+        }
+
+        [Fact]
+        public void ReturnTrueIfTheFileNotExists()
+        {
+            string filePath = System.IO.Path.GetFullPath(".");
+            var target = new Provider();
+            Assert.True(target.Exists(filePath));
         }
     }
 }
