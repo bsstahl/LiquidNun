1f8e67121204dfc99055f63aa2bf146c08740a49
Barry Stahl <barry.stahl@carvana.com>
2019-07-31 16:26:32 -0700

    Implement and publish HTTPClient.Native Provider.

---
+++ src/LiquidNun/Interfaces/IHttpClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiquidNun.Interfaces
+{
+    public interface IHttpClient
+    {
+        string GetString(string url);
+    }
+}
